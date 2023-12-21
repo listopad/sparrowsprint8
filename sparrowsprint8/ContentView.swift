a3dda0ed40f3eb0239634e84e922165bf8204048
Artem <adelelain@gmail.com>
2023-12-21 17:21:40 +0300

    Initial Commit

---
+++ sparrowsprint8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sparrowsprint8
+//
+//  Created by Artem Dragunov on 21.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sparrowsprint8/sparrowsprint8App.swift
@@ -0,0 +1,17 @@
+//
+//  sparrowsprint8App.swift
+//  sparrowsprint8
+//
+//  Created by Artem Dragunov on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct sparrowsprint8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
