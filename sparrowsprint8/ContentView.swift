df8a3a451a99dd680da073985cfb9eba892126d0
Artem <adelelain@gmail.com>
2023-12-22 19:34:29 +0300

    add static slider

--- sparrowsprint8/ContentView.swift
+++ sparrowsprint8/ContentView.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        @State var percentage: Float = 50
+        
         ZStack {
             Image("wallpaper")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
+            
+            Slider()
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct Slider: View {
+    private let sliderHeight: CGFloat = 200
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(.ultraThinMaterial)
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(.white)
+        }
+        .frame(width: 80, height: sliderHeight)
+        .gesture(DragGesture(minimumDistance: 0)
+            .onChanged({ value in
+                // Handle the drag gesture
+            }))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
